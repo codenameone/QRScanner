fa127bcdee3f4d365dfe54287768ed1f24913d0e
Steve Hannah <steve@weblite.ca>
2023-10-14 10:10:39 -0700

    automatically add ios permissions when running in simulator

--- javase/src/main/java/org/littlemonkey/qrscanner/NativeScannerImpl.java
+++ javase/src/main/java/org/littlemonkey/qrscanner/NativeScannerImpl.java
@@ -1,6 +1,8 @@
 package org.littlemonkey.qrscanner;
 
 public class NativeScannerImpl implements org.littlemonkey.qrscanner.NativeScanner{
+
+    private static boolean cameraUsageDescriptionChecked;
     public void scanQRCode() {
     }
 
@@ -8,7 +10,21 @@ public class NativeScannerImpl implements org.littlemonkey.qrscanner.NativeScann
     }
 
     public boolean isSupported() {
+        checkCameraUsageDescription();
         return false;
+    }
+
+    private static void checkCameraUsageDescription() {
+        if (!cameraUsageDescriptionChecked) {
+            cameraUsageDescriptionChecked = true;
+
+            java.util.Map<String, String> m = com.codename1.ui.Display.getInstance().getProjectBuildHints();
+            if(m != null) {
+                if(!m.containsKey("ios.NSCameraUsageDescription")) {
+                    com.codename1.ui.Display.getInstance().setProjectBuildHint("ios.NSCameraUsageDescription", "Some functionality of the application requires your camera");
+                }
+            }
+        }
     }
 
 }
